27a00deb3f0f282a84c2f6cd1d3b424382cde21f
Brant <bdm@brant.work>
2021-06-28 16:00:35 -0500

    Modified Index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
-export default function HomePage() {
+import Layout from '../components/Layout'
+import FilmListGrid from '../components/FilmListGrid'
+import { fetchMoviesByListId } from '../utils'
+
+export default function HomePage({creator, films}) {
   return (
-    <div>
-    </div>
+    <Layout>
+      <div className="mb-5 text-center text-3xl md:text-5xl">
+        <h2>My {films.length} Films List</h2>
+        <h3 className="text-xl md:text-2xl text-blue-50 text-opacity-70">Created by: {creator}</h3>
+      </div>
+      <FilmListGrid films={films} />
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const {creator, films } = await fetchMoviesByListId(process.env.MY_LIST_ID)
+
+  return {
+    props: {
+      creator,
+      films
+    }
+  }
 }
